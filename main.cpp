ff1aa3762d44e61343fd8da0190b48120edcf1f5
Rania Mahmouh <raniamahmouh6@gmail.com>
2026-01-21 18:14:39 +0000

    Ajout du code de filtrage

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include "Filtrage.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    Mat img = imread("image.png", IMREAD_GRAYSCALE);
+
+    if (img.empty())
+    {
+        cout << "Erreur : image non chargee" << endl;
+        return -1;
+    }
+
+    Filtrage f(img);
+
+    Mat moyenneur = f.filtreMoyenneur();
+    Mat gaussien = f.filtreGaussien();
+    Mat laplacien = f.filtreLaplacien();
+
+    f.afficher(img, "Original");
+    f.afficher(moyenneur, "Moyenne");
+    f.afficher(gaussien, "Gaussien");
+    f.afficher(laplacien, "Laplacien");
+
+    waitKey(0);
+
+    f.sauvegarder(moyenneur, "moyenne.png");
+    f.sauvegarder(gaussien, "gaussien.png");
+    f.sauvegarder(laplacien, "laplacien.png");
+
+    return 0;
+}
+
+
