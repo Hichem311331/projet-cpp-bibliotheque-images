93d6798a6c0fed9d2f3ed2ac9cc9f9326808bf22
mohcenehammoudi09-stack <mohcene.hammoudi09@gmail.com>
2026-01-23 00:32:55 +0100

    Update segmentation.hpp
    latest version

--- include/segmentation.hpp
+++ include/segmentation.hpp
@@ -1,28 +1,26 @@
 #ifndef SEGMENTATION_HPP
 #define SEGMENTATION_HPP
+
 #include <opencv2/opencv.hpp>
-#include <iostream>
 using namespace cv;
 class Segmentation {
 private:
     int K;
     int ITER;
-
     int N;
-    unsigned char* pixels; // img.data (image d'entrée)
-
+    unsigned char* pixels;
     int* labels;
     float* centers;
 
 public:
-    Segmentation(int k = 3, int iter = 10);
-    ~Segmentation();// destructeur :pour libertaion de la memoire
-
+    Segmentation(int k = 2, int iter=100);// 2 et 100 par defaut : le min a faire
+    ~Segmentation();// un desctructeur pour libérer la memoire
+    void demanderparams();            
     void prepare(const Mat& img);
-    // calcul du kmeans
-    void run();
-    // image resultante
-    Mat getResult(const Mat& img) const;
+    void calcul();
+    Mat resultat(const Mat& img) const;
 };
 
 #endif
+
+
