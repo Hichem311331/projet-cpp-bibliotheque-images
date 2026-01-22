68bc2a25c90eeb3574156ed2cc70bb1206967fa6
mohcenehammoudi09-stack <mohcene.hammoudi09@gmail.com>
2026-01-23 00:32:25 +0100

    Update segmentation.cpp
    latest version

--- src/algorithms/segmentation/segmentation.cpp
+++ src/algorithms/segmentation/segmentation.cpp
@@ -1,39 +1,55 @@
 #include "segmentation.hpp"
 #include <iostream>
-
+using namespace std; 
 using namespace cv;
+Segmentation::Segmentation(int k, int iter)// constructeur 
+    : K(k), ITER(iter), N(0),
+      pixels(nullptr), labels(nullptr), centers(nullptr) {}
 
-Segmentation::Segmentation(int k, int iter)//constructeur
-    : K(k), ITER(iter), N(0), pixels(nullptr), labels(nullptr), centers(nullptr) {}
-
-Segmentation::~Segmentation() {//destructeur
-    delete[] labels;
+Segmentation::~Segmentation() {
+    delete[] labels; // destru : libierer la memoire
     delete[] centers;
 }
 
-void Segmentation::prepare(const Mat& img) {
-    
-    N = img.rows * img.cols;
+void Segmentation::demanderparams() {
+cout << "Entrer le nombre de clusters (K) : ";
+cin >> K;
+
+cout << "Entrer le nombre d'iterations : ";
+cin >> ITER;
+
+if (K <= 0) {
+	cout << "K invalide, K = 2 par defaut\n";
+        K = 2;
+    }
+
+if (ITER <= 0) {
+	cout << "ITER invalide, ITER = 100 par defaut\n";
+        ITER = 100;
+    }
+}
+
+void Segmentation::prepare(const cv::Mat& img) {
+    N = img.rows * img.cols;// taillde de l'image
     pixels = img.data;
 
     delete[] labels;
     delete[] centers;
-
     labels = new int[N];
     centers = new float[K];
 
-    // init centres déterministe
-        for (int k = 0; k < K; k++) {
+    // Initialisation des centroides
+    for (int k = 0; k < K; k++) {
         int idx = (k * N) / K;
         centers[k] = pixels[idx];
     }
 }
 
-void Segmentation::run() {
+
+void Segmentation::calcul() {// calucl du Kmeans
     for (int it = 0; it < ITER; it++) {
 
-        // assignation
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {// attribution des labels
             float minDist = 1e9f;
             int best = 0;
 
@@ -42,16 +58,20 @@ void Segmentation::run() {
                 float dist = d * d;
                 if (dist < minDist) {
                     minDist = dist;
-                    best=k;
+                    best = k;
                 }
             }
             labels[i] = best;
         }
 
-        // MAJ des centres
+        // MàJ des centres
         float* sum = new float[K];
         int* count = new int[K];
-        for (int k = 0; k < K; k++) { sum[k] = 0.0f; count[k] = 0; }
+
+        for (int k = 0; k < K; k++) {
+            sum[k] = 0.0f;
+            count[k] = 0;
+        }
 
         for (int i = 0; i < N; i++) {
             int k = labels[i];
@@ -60,23 +80,23 @@ void Segmentation::run() {
         }
 
         for (int k = 0; k < K; k++) {
-            if (count[k] > 0) centers[k] = sum[k] / count[k];
+            if (count[k] > 0)
+                centers[k] = sum[k] / count[k];
         }
-
         delete[] sum;
-        delete[] count;
+        delete[] count;//liberation
     }
 }
 
-Mat Segmentation::getResult(const Mat& img) const {
-    Mat result = img.clone();   // creation de la nouvelle image ne modifie pas l'original
+Mat Segmentation::resultat(const Mat& img) const { //image segmentée
+    Mat result = img.clone(); // creation dune copie de limage
     unsigned char* out = result.data;
 
     for (int i = 0; i < N; i++) {
         float v = centers[labels[i]];
         if (v < 0) v = 0;
         if (v > 255) v = 255;
-        out[i] = (unsigned char)v;
+        out[i] = static_cast<unsigned char>(v);
     }
 
     return result;
